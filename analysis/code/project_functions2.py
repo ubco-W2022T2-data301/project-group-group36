605ce1e22b01ea4611b2bd4a3cc78fb9746dd7d0
LilDA00 <michael001024@gmail.com>
2023-04-12 15:20:51 -0700

    m6 changes

--- analysis/code/project_functions2.py
+++ analysis/code/project_functions2.py
@@ -2,6 +2,34 @@ import pandas as pd
 
 
 
+# Method Chaining
+MFdf1 = (
+    pd.read_csv('../data/raw/Walmart.csv')
+    .copy().drop(['Store', 'Temperature','Fuel_Price','CPI','Unemployment'], axis=1)
+    .assign(Date=pd.to_datetime(pd.read_csv('../data/raw/Walmart.csv')['Date'], dayfirst=True, yearfirst=False))    
+    .groupby('Date').mean().round()
+    .sort_values(by=['Date']).reset_index()
+    .reset_index()
+    .rename(columns={'index': 'Week#'})
+    .assign(Previous_Week_Sales=lambda x: x['Weekly_Sales'].shift(1))
+    .assign(WoW_Changes=lambda x: (x['Weekly_Sales'] - x['Previous_Week_Sales']) / x['Previous_Week_Sales'])
+    .assign(WoW_Changes=lambda x: x['WoW_Changes'].apply(lambda x: f"{x*100:.2f}%"))
+)
+
+# Wrangled dataset sorted by weekly sales
+MFdf2 = (MFdf1
+    .sort_values(by='Weekly_Sales', ascending=False)
+    .reset_index().copy().drop(['index'], axis=1)
+    .astype({'Week#': str})
+)
+
+MFdf_topSales = MFdf2.head(15)
+MFdf_worseSales = MFdf2.tail(15)
+
+MFdf_topSales
+
+
+# function of data wrangling
 def dataset_processing(url_or_path_to_csv_file, wrangling_method):
     '''
     This functions wrangles the dataset into a more convinient form during data analysis for the relationship between weekly sales, Holidays, WoW changes etc.
