ad9732d684fb8376be3f672f2ee76f91aeb20deb
fumiya1204 <fhayakaw@student.ubc.ca>
2023-04-12 16:02:16 -0700

    Updated my works

--- analysis/code/project_functions3.py
+++ analysis/code/project_functions3.py
@@ -1,11 +1,22 @@
+## Move method change
+import math
+
+grouped_data = (pd.read_csv('../data/raw/Walmart.csv')
+                .rename(columns={'CPI': 'inflation'})
+                .assign(avg_sales=lambda x: x.groupby(['inflation', 'Temperature'])['Weekly_Sales'].transform('mean'))
+                .assign(Temperature_C=lambda x: round((x['Temperature'] - 32) * 5/9))
+                .assign(avg_sales_ceil=lambda x: x['avg_sales'].apply(lambda y: math.ceil(y)))
+                .groupby(['Temperature_C', 'inflation'])
+                .agg({'avg_sales_ceil': 'mean'})
+                .reset_index())
 
 ## Move method change to .py fille
 import pandas as pd
 import math
 
-def process_data(file_path):
+def process_data('../data/raw/Walmart.csv'):
     grouped_data = (
-        pd.read_csv(file_path)
+        pd.read_csv('../data/raw/Walmart.csv')
           .rename(columns={'CPI': 'inflation'})
           .assign(avg_sales=lambda x: x.groupby(['inflation', 'Temperature'])['Weekly_Sales'].transform('mean'))
           .assign(Temperature_C=lambda x: round((x['Temperature'] - 32) * 5/9))
